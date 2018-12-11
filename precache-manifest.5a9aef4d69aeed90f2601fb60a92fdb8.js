fc7c6e810aa3e0ac7b19fb90039cf9fff6070425
Ryan Cullum <racullum@gmail.com>
2018-12-10 22:57:24 -0600

    Updates

---
+++ precache-manifest.5a9aef4d69aeed90f2601fb60a92fdb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d38c069875269695811d",
+    "url": "/todo/static/css/main.61448803.chunk.css"
+  },
+  {
+    "revision": "d38c069875269695811d",
+    "url": "/todo/static/js/main.d38c0698.chunk.js"
+  },
+  {
+    "revision": "939cbde5f9c76620fa49",
+    "url": "/todo/static/js/1.939cbde5.chunk.js"
+  },
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "19f9bcc017e7e599b6a597e724407d17",
+    "url": "/todo/index.html"
+  }
+];
